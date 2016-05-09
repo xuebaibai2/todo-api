c0e6ee1293422c7da0c14c244b34b64d212b057c
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-09 17:21:25 +1000

    PUT function

--- server.js
+++ server.js
@@ -62,7 +62,39 @@ app.delete('/todos/:id', function (req, res) {
         todos = _.without(todos, matchedTodo);
         res.send("Item is deleted!");
     }
-})
+});
+
+//PUT /todos/:id
+app.put('/todos/:id',function (req, res) {
+    var todoID = parseInt(req.params.id);
+    var matchedTodo = _.findWhere(todos, {id: todoID});
+    var body = _.pick(req.body, 'description', 'completed');
+    var validAttributes = {};
+
+    if (!matchedTodo){
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    }else if (body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    }else{
+
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+        validAttributes.description = body.description;
+    }else if (body.hasOwnProperty('description')){
+        return res.status(400).send();
+    }else{
+
+    }
+
+    _.extend(matchedTodo, validAttributes);
+    res.json(matchedTodo);
+
+});
 
 app.listen(PORT, function () {
     console.log("Express listing on port: " + PORT) + "!";
