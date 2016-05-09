307c42144c87066718e691d2d69580e4729f8e35
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-09 16:46:17 +1000

    Underscore

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
  */
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,22 +23,29 @@ app.get('/todos', function (req, res) {
 //GET /todos/:id
 app.get('/todos/:id', function (req, res) {
     var todoID = parseInt(req.params.id);
-    var todoList = [];
-    todos.forEach(function (task) {
-        if (task.id === todoID) {
-            todoList.push(task);
-        }
-    });
-    if (todoList.length == 0) {
+    var matchedTodo = _.findWhere(todos,{id: todoID});
+
+    // var todoList = [];
+    // todos.forEach(function (task) {
+    //     if (task.id === todoID) {
+    //         todoList.push(task);
+    //     }
+    // });
+    if (!matchedTodo) {
         res.status(404).send("Result not found!");
     } else {
-        res.send(todoList);
+        res.send(matchedTodo);
     }
 });
 
 //POST /todos
 app.post('/todos',function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body, 'description', 'completed');
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
     body.id = todoNextId++;
     todos.push(body);
     res.json(todos);
