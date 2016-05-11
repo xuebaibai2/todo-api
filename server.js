847d4f763b86a35273d11662b47ebed07a81adc5
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-11 23:37:46 +1000

    Add auth token generation to POST /users/login

--- server.js
+++ server.js
@@ -155,7 +155,13 @@ app.post('/users/login', function (req, res) {
 
     db.user.authenticate(body)
         .then(function (user) {
-            res.json(user.toPublicJSON());
+            var token = user.generateToken('authentication');
+            if (token){
+                res.header('Auth', token).json(user.toPublicJSON());
+            }else{
+                res.status(401).send();
+            }
+
         }, function (e) {
             res.status(401).json(e);
         });
