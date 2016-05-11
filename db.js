4c3f0a025245c7eacc0f2207ea22202807c72e89
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-11 14:45:05 +1000

    Add postgrest for production

--- db.js
+++ db.js
@@ -2,10 +2,19 @@
  * Created by Cayden on 16/5/9.
  */
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env = process.env.NODE_ENV || 'development';
+var sequelize;
+if (env === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres'
+    });
+} else {
+    sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
+
 
 var db = {};
 
