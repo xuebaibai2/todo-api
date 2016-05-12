9e249ba8012e4bd16708f60665d0b8aa3478b3a0
Yanfeng Li <xuebaibai2@hotmail.com>
2016-05-12 17:55:21 +1000

    database sync force:true

--- server.js
+++ server.js
@@ -181,7 +181,9 @@ app.post('/users/login', function (req, res) {
         });
 });
 
-db.sequelize.sync().then(function () {
+db.sequelize.sync({
+    force: true
+}).then(function () {
     app.listen(PORT, function () {
         console.log("Express listing on port: " + PORT) + "!";
     });
